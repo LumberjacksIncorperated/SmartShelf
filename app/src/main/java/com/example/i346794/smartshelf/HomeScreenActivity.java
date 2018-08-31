414d58d239a4353f39f62b2711e76bc5dc75a52a
Robert Charlton <robcharlton@sap.com>
2018-08-31 16:00:30 +1000

    unused imports removed from java classes

--- app/src/main/java/com/example/i346794/smartshelf/HomeScreenActivity.java
+++ app/src/main/java/com/example/i346794/smartshelf/HomeScreenActivity.java
@@ -14,10 +14,6 @@ package com.example.i346794.smartshelf;
 
 import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 public class HomeScreenActivity extends BaseActivity {
 
